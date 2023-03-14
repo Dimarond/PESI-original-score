e49654c6ff76f655efebd371216d6a23882e9c3d
UbungMachtMeister <59809057+Dimarond@users.noreply.github.com>
2023-03-14 18:27:34 +0200

    Update PESI_original_score_calculator.py4

--- PESI_original_score_calculator.py
+++ PESI_original_score_calculator.py
@@ -1,6 +1,28 @@
-'''Ask user questions from pesi table:Vârstă Sex masculin Cancer Insuficienţă cardiacă congestivă Boală pulmonară cronică Alură ventriculară >110/min Tensiune arterială sistolică <100 mmHg Frecvenţă respirato-rie >30/min Temperatură <36°C Status mental alterat Saturaţia oxigenului arterial <90% Stratificarea riscului' '''
+'''Ask user questions from pesi table:
 
-     print('age?')
+Age = int(input("What is the patient's age? ")
+
+Male = input("Is the patient male? (yes/NO) ")
+
+
+Cancer = input("Does the patient have cancer? ")
+
+Congestive_Heart_Failure = 
+
+Boală pulmonară cronică 
+
+Alură ventriculară >110/min 
+
+Tensiune arterială sistolică <100 mmHg
+
+Frecvenţă respirato-rie >30/min
+ 
+Temperatură <36°C 
+
+Status mental alterat 
+
+Saturaţia oxigenului arterial <90% '''
+
 
 
 '''Get imput for each question'''
