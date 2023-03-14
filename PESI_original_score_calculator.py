163c2961b1e9ed03ce21c09ddb0e294c711c2cc2
UbungMachtMeister <59809057+Dimarond@users.noreply.github.com>
2023-03-14 11:42:58 +0200

    Create PESI_original_score_calculator.py

---
+++ PESI_original_score_calculator.py
@@ -0,0 +1,10 @@
+'''Ask user questions from pesi table:Vârstă Sex masculin Cancer Insuficienţă cardiacă congestivă Boală pulmonară cronică Alură ventriculară >110/min Tensiune arterială sistolică <100 mmHg Frecvenţă respirato-rie >30/min Temperatură <36°C Status mental alterat Saturaţia oxigenului arterial <90% Stratificarea riscului' '''
+
+'''Get imput for each question'''
+
+'''Calculate value of each answer'''
+
+
+'''Add all the points for a total'''
+
+'''Output and print the risk class'''
